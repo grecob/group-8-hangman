dd4f0cdf43c9a285dc6d541cdb0d10e7d39b7669
grecob <bryson.greco@gmail.com>
2024-02-25 19:34:10 -0600

    update function to allow for exporting class instance

--- Dictionary.py
+++ Dictionary.py
@@ -3,10 +3,12 @@ import random
 from ChosenWord import ChosenWord
 #temp file name
 class Dictionary:  
-
+    chosen_word = ChosenWord()
     chosen_category = ''
     random_number = 0
     
+    def get_chosen_word_instance(self):
+        return self.chosen_word
     
     def choose_category(self, category):
         catalog_of_categories = {
@@ -20,7 +22,6 @@ class Dictionary:
         self.random_number = random.randrange(0, 5)
 
     def read_csv(self):
-        chosen_word = ChosenWord()
         row_number = 0
         self.randomization()
         with open(self.choosen_category, mode ='r')as csv_file:
@@ -30,12 +31,9 @@ class Dictionary:
                 if row_number == self.random_number:
                     line = str(row)
                     split_line = line.split(',')               
-                    chosen_word.set_word(split_line[0])
-                    chosen_word.set_hint1(split_line[1])
-                    chosen_word.set_hint2(split_line[2])
-                    chosen_word.set_hint3(split_line[3])
+                    self.chosen_word.set_word(split_line[0])
+                    self.chosen_word.set_hint1(split_line[1])
+                    self.chosen_word.set_hint2(split_line[2])
+                    self.chosen_word.set_hint3(split_line[3])
                     break
                 row_number = row_number + 1
-
-        
-
