356eb416c3bb84f55c49c5472b3fa1b01e75136b
grecob <bryson.greco@gmail.com>
2024-04-02 08:28:04 -0500

    US6 features

--- ChosenWord.py
+++ ChosenWord.py
@@ -1,24 +1,8 @@
 class ChosenWord:
 
     word = None
-    hint1 = None
-    hint2 = None
-    hint3 = None
 
     def set_word(self, chosen_word):
         self.word = chosen_word
     def get_word(self):
         return self.word
-    def set_hint1(self, hint1):
-        self.hint1 = hint1
-    def get_hint1(self):
-        return self.hint1
-    def set_hint2(self, hint2):
-        self.hint2 = hint2
-    def get_hint2(self):
-        return self.hint2
-    def set_hint3(self, hint3):
-        self.hint3 = hint3
-    def get_hint3(self):
-        return self.hint3
-    
---
+++ Words.py
@@ -0,0 +1,4 @@
+class Words:
+    easy = ["Apple", "Ball", "Cat", "Dog", "Egg","Fish", "Goat", "Hat", "Ice", "Jar", "Kite", "Lion", "Moon", "Nest", "Owl", "Pig", "Quiz", "Rat", "Sun", "Tree"]
+    medium = ["Banana", "Elephant", "Guitar", "Helicopter", "Igloo", "Lantern", "Mystery", "Necklace", "Octopus", "Penguin", "Quilt", "Rainbow", "Squirrel", "Telescope", "Umbrella", "Volcano", "Whisper", "Xylophone", "Yacht", "Zebra"]
+    hard = ["Anthropology", "Bureaucracy", "Circumnavigate", "Discombobulate", "Exacerbate", "Flabbergasted", "Gerrymander", "Hippopotamus", "Inconsequential", "Juxtapose", "Kaleidoscopic", "Labyrinthine", "Misanthrope", "Nostalgia", "Onomatopoeia", "Procrastinate", "Quasar", "Rhapsody", "Serendipity", "Tintinnabulation"]
